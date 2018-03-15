a269ae6b9a0d9c0c006050e3e3c9d8a23c1fc00a
Stewart Mbofana <stewart.mbofana@imqs.co.za>
2018-03-15 16:30:30 +0200

    Modify users sql

--- auth/auth.sql
+++ auth/auth.sql
@@ -1,36 +1,36 @@
-CREATE DATABASE openid_connect_db;
+CREATE DATABASE openid_auth;
 
 CREATE TABLE users (
-	id           			SERIAL,
-	user_name    			VARCHAR(100) NOT NULL,
-	first_name   			VARCHAR(60)  NOT NULL,
-	last_name    			VARCHAR(60)  NOT NULL,
-	middle_name 			VARCHAR(60)  NULL,
-	nick_name 				VARCHAR(60)  NULL,
-	preferred_username 		VARCHAR(60)  NULL,
-	profile 				VARCHAR(200)  NULL,
-	picture 				VARCHAR(200)  NULL,
-	website        			VARCHAR(200) NULL,
-	email        			VARCHAR(200) NOT NULL,
-	email_verified      	boolean NULL,
-	gender      			VARCHAR(20) NULL,
-	birthdate      			VARCHAR(30) NULL,
-	zoneinfo      			VARCHAR(200) NULL,
-	phone_number 			VARCHAR(200) NULL,
-	phone_number_verified 	boolean NULL,
-	address 				JSONB NULL,
-	updated_at   			numeric NOT NULL,
+  id                    SERIAL,
+  user_name             VARCHAR(60)  NOT NULL,
+  first_name            VARCHAR(60)  NOT NULL,
+  last_name             VARCHAR(60)  NOT NULL,
+  middle_name           VARCHAR(60)  NULL,
+  nick_name             VARCHAR(60)  NULL,
+  preferred_username    VARCHAR(60)  NULL,
+  profile               VARCHAR(200) NULL,
+  picture               VARCHAR(200) NULL,
+  website               VARCHAR(200) NULL,
+  email                 VARCHAR(200) NOT NULL,
+  email_verified        boolean      NULL,
+  gender                VARCHAR(20)  NULL,
+  birthdate             VARCHAR(30)  NULL,
+  zoneinfo              VARCHAR(100) NULL,
+  phone_number          VARCHAR(20)  NULL,
+  phone_number_verified boolean      NULL,
+  address_id            INT          NULL,
+  updated_at            numeric      NOT NULL,
   CONSTRAINT user_id_pkey PRIMARY KEY (id)
 );
 
-create  table address(
-	id 					SERIAL,
-	street_address 		VARCHAR(200) NULL,
-	city 				VARCHAR(100) NULL,
-	region 				VARCHAR(60) NULL,
-	postal_code 		VARCHAR(20) NULL,
-	country 			VARCHAR(100) NULL,
-	CONSTRAINT address_id_pkey PRIMARY KEY (id)
+create table address (
+  id             SERIAL,
+  street_address VARCHAR(200) NULL,
+  city           VARCHAR(100) NULL,
+  region         VARCHAR(60)  NULL,
+  postal_code    VARCHAR(20)  NULL,
+  country        VARCHAR(100) NULL,
+  CONSTRAINT address_id_pkey PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
